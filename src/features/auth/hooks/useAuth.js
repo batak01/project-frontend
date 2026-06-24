be910a96a31630689eabddf265459abc1205777a
Pebrin Pardede <pardedepebrin@gmail.com>
2026-06-25 00:11:28 +0700

    refactor: restructure auth feature architecture

--- src/features/auth/hooks/useAuth.js
+++ src/features/auth/hooks/useAuth.js
@@ -1,7 +1,7 @@
 "use client";
 
 import { useContext } from "react";
-import AuthContext from "@/context/AuthContext";
+import AuthContext from "@/contexts/AuthContext";
 
 const useAuth = () => {
   return useContext(AuthContext);
